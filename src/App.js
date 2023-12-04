ba531249fe00b0f9bc1c341c5f158ab7aaf2d2b6
behzod <behzod4214974@gmail.com>
2023-12-04 18:14:11 +0500

    fetch is added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,20 @@ import Header from "./components/header/header";
 import Main from "./components/main/main";
 
 function App() {
-	let [isWhite,setWhite] = useState(false)
+  let [countryData, setData] = useState([]);
+  let [isWhite, setWhite] = useState(false);
+
+  fetch(" https://restcountries.com/v3.1/all")
+    .then((res) => res.json())
+    .then((data) => setData(data))
+	.catch(error => console.error(error))
+
   return (
-	<div className={`body  ${isWhite ? 'body-white-mode' : '' } `}>
-	<Header whiteMode={isWhite} setWhite={setWhite}/>
-	<Main/>
-	</div>
-  )
+    <div className={`body  ${isWhite ? "body-white-mode" : ""} `}>
+      <Header whiteMode={isWhite} setWhite={setWhite} />
+      <Main countries={countryData} />
+    </div>
+  );
 }
 
 export default App;
